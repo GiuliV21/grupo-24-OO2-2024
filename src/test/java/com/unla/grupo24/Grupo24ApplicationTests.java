7ca86ac39f3f7414a4243fac68227a07660e7966
Giuliana Vera <107806539+GiuliV21@users.noreply.github.com>
2024-06-17 23:44:19 -0300

    Add files via upload

---
+++ src/test/java/com/unla/grupo24/Grupo24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
